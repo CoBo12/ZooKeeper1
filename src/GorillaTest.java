16964d887ca35065897488fa0750d3d327e5c47b
Corbin Bosley <corbinbosley@gmail.com>
2020-12-19 17:56:26 -0500

    Zoo keeper part 1 assignment

---
+++ src/GorillaTest.java
@@ -0,0 +1,19 @@
+
+public class GorillaTest {
+
+	public static void main(String[] args) {
+		Gorilla sampson = new Gorilla();
+		sampson.throwSomething();
+		sampson.showEnergyLevel();
+		sampson.throwSomething();
+		sampson.showEnergyLevel();
+		sampson.throwSomething();
+		sampson.showEnergyLevel();
+		sampson.eatBanana();
+		sampson.eatBanana();
+		sampson.climb();
+		sampson.showEnergyLevel();
+
+	}
+
+}
---
+++ src/Mammal.java
@@ -0,0 +1,23 @@
+
+public class Mammal {
+	private int energyLevel = 100;
+	
+	
+	
+	public int showEnergyLevel() {
+		System.out.println("Energy level :" + this.getEnergyLevel());
+		return this.getEnergyLevel();
+	}
+
+
+
+	public int getEnergyLevel() {
+		return energyLevel;
+	}
+
+
+
+	public void setEnergyLevel(int energyLevel) {
+		this.energyLevel = energyLevel;
+	}
+}
